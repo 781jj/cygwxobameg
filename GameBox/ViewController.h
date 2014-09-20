7e7aaadf205c763a410a3fc0192f1ab10b5bf53c
Yao Ming <Chris.yaoming>
2014-09-20 10:47:20 +0800

    Initial Commit

---
+++ GameBox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GameBox
+//
+//  Created by YaoMing on 14-9-20.
+//  Copyright (c) 2014年 cyggame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GameBox/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GameBox
+//
+//  Created by YaoMing on 14-9-20.
+//  Copyright (c) 2014年 cyggame. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
