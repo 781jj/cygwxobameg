a853fa05ec2eef2a1f1a125c3d284ece188c8cf1
guxianxiong <guxianixong@163.com>
2014-10-09 18:50:40 +0800

    1. facebook 登录

--- GameBox/View/VSLoginViewController.m
+++ GameBox/View/VSLoginViewController.m
@@ -10,8 +10,11 @@
 #import "VSSessionManager.h"
 #import "VSTempLoginMessage.h"
 #import "VSCommon.h"
-@interface VSLoginViewController ()
+#import <FacebookSDK/FacebookSDK.h>
 
+
+@interface VSLoginViewController ()<FBLoginViewDelegate>
+@property(nonatomic, strong)FBLoginView *loginView;
 @end
 
 @implementation VSLoginViewController
@@ -19,6 +22,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _loginView = [[FBLoginView alloc] initWithReadPermissions:@[@"public_profile"]];
+    _loginView.delegate = self;
+    _loginView.hidden = YES;
+    [self.view addSubview:_loginView];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,7 +38,35 @@
 
 - (IBAction)facebookLogin:(id)sender
 {
+    _loginView.center = self.view.center;
+    _loginView.hidden = NO;
     
+    
+//    // If the session state is any of the two "open" states when the button is clicked
+//    if (FBSession.activeSession.state == FBSessionStateOpen
+//        || FBSession.activeSession.state == FBSessionStateOpenTokenExtended) {
+//        
+//        // Close the session and remove the access token from the cache
+//        // The session state handler (in the app delegate) will be called automatically
+//        [FBSession.activeSession closeAndClearTokenInformation];
+//        
+//        // If the session state is not any of the two "open" states when the button is clicked
+//    } else {
+//        // Open a session showing the user the login UI
+//        // You must ALWAYS ask for public_profile permissions when opening a session
+//        [FBSession openActiveSessionWithReadPermissions:@[@"public_profile"]
+//                                           allowLoginUI:YES
+//                                      completionHandler:
+//         ^(FBSession *session, FBSessionState state, NSError *error) {
+//             
+//             // Retrieve the app delegate
+//             AppDelegate* appDelegate = [UIApplication sharedApplication].delegate;
+//             // Call the app delegate's sessionStateChanged:state:error method to handle session state changes
+//             [appDelegate sessionStateChanged:session state:state error:error];
+//         }];
+//    }
+    
+
 }
 
 - (IBAction)twitterLogin:(id)sender
@@ -53,14 +90,63 @@
 }
 
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - FBLoginViewDelegate methods
+-(void)loginView:(FBLoginView *)loginView handleError:(NSError *)error
+{
+    NSString *alertMessage, *alertTitle;
+    
+    // If the user should perform an action outside of you app to recover,
+    // the SDK will provide a message for the user, you just need to surface it.
+    // This conveniently handles cases like Facebook password change or unverified Facebook accounts.
+    if ([FBErrorUtility shouldNotifyUserForError:error]) {
+        alertTitle = @"Facebook error";
+        alertMessage = [FBErrorUtility userMessageForError:error];
+        
+        // This code will handle session closures that happen outside of the app
+        // You can take a look at our error handling guide to know more about it
+        // https://developers.facebook.com/docs/ios/errors
+    } else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryAuthenticationReopenSession) {
+        alertTitle = @"Session Error";
+        alertMessage = @"Your current session is no longer valid. Please log in again.";
+        
+        // If the user has cancelled a login, we will do nothing.
+        // You can also choose to show the user a message if cancelling login will result in
+        // the user not being able to complete a task they had initiated in your app
+        // (like accessing FB-stored information or posting to Facebook)
+    } else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled) {
+        NSLog(@"user cancelled login");
+        
+        // For simplicity, this sample handles other errors with a generic message
+        // You can checkout our error handling guide for more detailed information
+        // https://developers.facebook.com/docs/ios/errors
+    } else {
+        alertTitle  = @"Something went wrong";
+        alertMessage = @"Please try again later.";
+        NSLog(@"Unexpected error:%@", error);
+    }
+    
+    if (alertMessage) {
+        [[[UIAlertView alloc] initWithTitle:alertTitle
+                                    message:alertMessage
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
 }
-*/
+
+-(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
+{
+    NSLog(@":%@", user);
+}
+
+-(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView
+{
+    NSLog(@"gxx");
+}
+
+-(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
+{
+    
+}
 
 @end
